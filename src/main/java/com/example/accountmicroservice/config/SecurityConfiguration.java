8feb7e79a56314f1437af9cc60a2064040cb998f
xYarvinx <otebator@gmail.com>
2024-10-15 01:43:34 +1000

    Some fixes(part don't work)

--- src/main/java/com/example/accountmicroservice/config/SecurityConfiguration.java
+++ src/main/java/com/example/accountmicroservice/config/SecurityConfiguration.java
@@ -24,7 +24,7 @@ public class SecurityConfiguration {
     private final CustomAccessDeniedHandler customAccessDeniedHandler;
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain accountFilterChain(HttpSecurity http) throws Exception {
         http.csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/Authentication/**",
